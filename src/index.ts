f0462636b5498b25d33cbd009dc7843c14192202
Emmanuel Benson <emmanuel.benson@konga.com>
2022-02-24 10:00:34 +0100

    Update map marker

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-// import { User } from "./User";
-// import { Company } from "./company";
+import { User } from "./User";
+import { Company } from "./company";
 import { CustomMap } from "./customMap";
 
-new CustomMap('map');
+const customMap = new CustomMap('map');
+const user = new User();
+const company = new Company();
+
+customMap.addMarker(user);
+customMap.addMarker(company);
